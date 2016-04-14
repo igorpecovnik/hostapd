3bf22e2a43517ef8be28386ee6bc44b3a87552a7
Igor Pecovnik <igor.pecovnik@gmail.com>
2016-04-14 20:42:36 +0200

    Complete script rework, now packing both binaries (default and realtek), example configs, milti platform

--- go.sh
+++ go.sh
@@ -10,11 +10,11 @@
 #
 
 
-#TARGET="-realtek"							# For (most) realtek use "realtek"
-#STABLE="yes"								# Comment this out to use development version
-STABLETAG="hostap_2_5"
-MAINTAINER="Igor Pecovnik"                  		# deb signature
-MAINTAINERMAIL="igor.pecovnik@****l.com"    		# deb signature 
+[[ $MAINTAINER == "" ]] && MAINTAINER="Igor Pecovnik"                                   # deb signature
+[[ $MAINTAINERMAIL == "" ]] && MAINTAINERMAIL="igor.pecovnik@****l.com"                 # deb signature 
+[[ $REVISION == "" ]] && REVISION="1.0"
+[[ $ARCHITECTURE == "" ]] && ARCHITECTURE=$(dpkg --print-architecture)
+
 
 #--------------------------------------------------------------------------------------------------------------------------------
 # Set prerequisities and cleanup
@@ -22,7 +22,7 @@ MAINTAINERMAIL="igor.pecovnik@****l.com"    		# deb signature
 SRC=$(pwd)
 CPUS=$(grep -c 'processor' /proc/cpuinfo) 
 CTHREADS="-j$(($CPUS + $CPUS/2))"; 
-rm -f buildlog
+rm -f build.log
 rm -f hostapd-armbian*.deb
 
 
@@ -30,7 +30,7 @@ rm -f hostapd-armbian*.deb
 # Download some dependencies
 #--------------------------------------------------------------------------------------------------------------------------------
 echo -e "[\e[0;32m o.k. \x1B[0m] Building hostapd$TARGET"
-echo -e "[\e[0;32m o.k. \x1B[0m] Downloading dependencies. Please wait!"
+echo -e "[\e[0;32m o.k. \x1B[0m] Downloading dependencies."
 apt-get -qq -y install libnl-3-dev libssl-dev libnl-genl-3-dev
 
 
@@ -39,22 +39,28 @@ apt-get -qq -y install libnl-3-dev libssl-dev libnl-genl-3-dev
 #--------------------------------------------------------------------------------------------------------------------------------
 if [ -d "$SRC/hostap" ]; then
 		cd $SRC/hostap
-		git checkout -f -q master
+		git checkout -f -q master >> ../build.log 2>&1
 		git pull -q
-		echo -e "[\e[0;32m o.k. \x1B[0m] Updating sources. Please wait!"
+		echo -e "[\e[0;32m o.k. \x1B[0m] Updating sources."
 	else
-		git clone -q git://w1.fi/hostap.git 
+		git clone -q git://w1.fi/hostap.git >> ../build.log 2>&1 
 fi
 
 
+checkout ()
+{
 #--------------------------------------------------------------------------------------------------------------------------------
 # Choose stable branch if selected
 #--------------------------------------------------------------------------------------------------------------------------------
-if [ "$STABLE" == "yes" ]; then
+if [ "$1" == "stable" ]; then
 	cd $SRC/hostap
-	git checkout -f -q $STABLETAG
+	git checkout -f -q "hostap_2_5" >> ../build.log 2>&1
+	else
+	git checkout -f -q >> ../build.log 2>&1
 fi
-
+# Read version
+VERSION=$(cat $SRC/hostap/src/common/version.h | grep "#define VERSION_STR " | awk '{ print $3 }' | sed 's/\"//g')
+}
 
 #--------------------------------------------------------------------------------------------------------------------------------
 # Copy Driver interface for rtl871x driver
@@ -63,12 +69,11 @@ cp $SRC/files/*.* $SRC/hostap/src/drivers/
 cd $SRC/hostap/
 
 
-#--------------------------------------------------------------------------------------------------------------------------------
-# Read version
-#--------------------------------------------------------------------------------------------------------------------------------
-VERSION=$(cat $SRC/hostap/src/common/version.h | grep "#define VERSION_STR " | awk '{ print $3 }' | sed 's/\"//g')
 
 
+
+patching ()
+{
 #--------------------------------------------------------------------------------------------------------------------------------
 # Patching
 #
@@ -77,10 +82,10 @@ VERSION=$(cat $SRC/hostap/src/common/version.h | grep "#define VERSION_STR " | a
 #--------------------------------------------------------------------------------------------------------------------------------
 # brute force for 40Mhz
 if [ "$(patch --dry-run -t -p1 < $SRC/patch/300-noscan.patch | grep previ)" == "" ]; then
-	patch --batch -f -p1 < $SRC/patch/300-noscan.patch > ../build.log 2>&1
+	patch --batch -f -p1 < $SRC/patch/300-noscan.patch >> ../build.log 2>&1
 fi
 # patch for realtek
-if [ "$TARGET" == "-realtek" ]; then
+if [ "$1" == "-realtek" ]; then
 		cp $SRC/config/config_realtek $SRC/hostap/hostapd/.config
 		patch --batch -f -p1 < $SRC/patch/realtek.patch >> ../build.log 2>&1
 	else
@@ -89,44 +94,58 @@ if [ "$TARGET" == "-realtek" ]; then
 			patch --batch -t -p1 < $SRC/patch/realtek.patch >> ../build.log 2>&1
 		fi
 fi
+}
 
-
+compiling ()
+{
 #--------------------------------------------------------------------------------------------------------------------------------
 # Compile
 #--------------------------------------------------------------------------------------------------------------------------------
-cd hostapd
 make clean >/dev/null 2>&1
-echo -e "[\e[0;32m o.k. \x1B[0m] Compiling v$VERSION. Please wait!"
+echo -e "[\e[0;32m o.k. \x1B[0m] Compiling v$VERSION""$1."
 make $CTHREADS >> ../../build.log 2>&1
 if [ $? -ne 0 ] || [ ! -f $SRC/hostap/hostapd/hostapd ]; then
 	echo -e "[\e[0;31m err. \x1B[0m] hostapd not built."
         exit 1
 fi
+}
 
+checkout "stable"
+
+cd hostapd
+patching "-realtek"
+compiling " for Realteks"
+mv "$SRC/hostap/hostapd/hostapd" $SRC/hostap/hostapd/hostapd-rt
+mv "$SRC/hostap/hostapd/hostapd_cli" $SRC/hostap/hostapd/hostapd_cli-rt
+patching ""
+compiling
 
 #--------------------------------------------------------------------------------------------------------------------------------
 # Pack to deb. Replacec files in original package
 #--------------------------------------------------------------------------------------------------------------------------------
 cd $SRC
 apt-get -qq -d install hostapd
-dpkg-deb -R /var/cache/apt/archives/hostapd* hostapd-armbian$TARGET
+dpkg-deb -R /var/cache/apt/archives/hostapd* hostapd-armbian${TARGET}"_"${REVISION}_${ARCHITECTURE}
+
 # set up control file
-cat <<END > hostapd-armbian$TARGET/DEBIAN/control
+cat <<END > hostapd-armbian${TARGET}_${REVISION}_${ARCHITECTURE}/DEBIAN/control
 Package: hostapd-armbian$TARGET
-Version: $VERSION
-Architecture: armhf
+Version: $REVISION
+Architecture: $ARCHITECTURE
 Maintainer: $MAINTAINER <$MAINTAINERMAIL>
 Installed-Size: 1
 Section: kernel
+Conflicts: hostapd
 Priority: optional
 Description: Sources: https://github.com/igorpecovnik/hostapd
 END
 #
 
-cp $SRC/hostap/hostapd/hostapd* hostapd-armbian$TARGET/usr/sbin
-cd hostapd-armbian$TARGET
+cp "$SRC/hostap/hostapd/hostapd" "$SRC/hostap/hostapd/hostapd-rt" "$SRC/hostap/hostapd/hostapd_cli" "$SRC/hostap/hostapd/hostapd_cli-rt"  hostapd-armbian${TARGET}_${REVISION}_${ARCHITECTURE}/usr/sbin
+cp $SRC/hostapd.conf/*.conf hostapd-armbian${TARGET}_${REVISION}_${ARCHITECTURE}/etc
+cd hostapd-armbian${TARGET}_${REVISION}_${ARCHITECTURE}
 find . -type f ! -regex '.*.hg.*' ! -regex '.*?debian-binary.*' ! -regex '.*?DEBIAN.*' -printf '%P ' | xargs md5sum > DEBIAN/md5sums
 cd ..
-dpkg -b hostapd-armbian$TARGET >/dev/null 2>&1
-rm -rf hostapd-armbian$TARGET
-echo -e "[\e[0;32m o.k. \x1B[0m] All done. Hostapd is packed into: hostapd-armbian$TARGET.deb"
+dpkg -b hostapd-armbian${TARGET}_${REVISION}_${ARCHITECTURE} >/dev/null 2>&1
+rm -rf hostapd-armbian${TARGET}_${REVISION}_${ARCHITECTURE}
+echo -e "[\e[0;32m o.k. \x1B[0m] All done. Hostapd is packed into: hostapd-armbian${TARGET}_${REVISION}_${ARCHITECTURE}.deb"
