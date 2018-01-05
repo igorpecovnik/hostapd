3736f62bc41b2fcaec09ce56be951c0be46f7dbd
Igor Pecovnik <igor.pecovnik@gmail.com>
2018-01-05 13:15:40 +0100

    Bugfixes, adding few patches, removing building realtek, small rfc

--- README.md
+++ README.md
@@ -2,24 +2,12 @@
 
 This is a script to compile, patch and pack hostapd deamon **from sources**. It can be used as a drop-in replacement **for Debian and Ubuntu** based distributions.
 
-You can choose between four combinations:
-
-- development 
-- stable 
-
-and
-
-- default / all others  
-- Realtek adapters
-
-In all cases you get a new option **to control HT coexistance separate from noscan** - to force 40Mhz channels.
+Check included patches in patch/
 
 
-```bash
-sudo apt-get -y install git
-cd ~
-git clone https://github.com/igorpecovnik/hostapd
-chmod +x ./hostapd/go.sh
-cd hostapd
-./go.sh
-```
+	sudo apt-get -y install git
+	cd ~
+	git clone https://github.com/igorpecovnik/hostapd
+	chmod +x ./hostapd/go.sh
+	cd hostapd
+	./go.sh
--- go.sh
+++ go.sh
@@ -11,7 +11,7 @@
 
 
 [[ $MAINTAINER == "" ]] && MAINTAINER="Igor Pecovnik"                                   # deb signature
-[[ $MAINTAINERMAIL == "" ]] && MAINTAINERMAIL="igor.pecovnik@****l.com"                 # deb signature 
+[[ $MAINTAINERMAIL == "" ]] && MAINTAINERMAIL="igor.pecovnik@****l.com"                 # deb signature
 [[ $REVISION == "" ]] && REVISION="1.0"
 [[ $ARCHITECTURE == "" ]] && ARCHITECTURE=$(dpkg --print-architecture)
 [[ $SRC == "" ]] && SRC=$(pwd)
@@ -20,10 +20,9 @@
 # Set prerequisities and cleanup
 #--------------------------------------------------------------------------------------------------------------------------------
 
-CPUS=$(grep -c 'processor' /proc/cpuinfo) 
-CTHREADS="-j$(($CPUS + $CPUS/2))"; 
-rm -f build.log
-rm -f hostapd-armbian*.deb
+CPUS=$(grep -c 'processor' /proc/cpuinfo)
+CTHREADS="-j$(($CPUS + $CPUS/2))";
+rm -f build.log hostapd-custom*.deb
 
 
 #--------------------------------------------------------------------------------------------------------------------------------
@@ -31,13 +30,13 @@ rm -f hostapd-armbian*.deb
 #--------------------------------------------------------------------------------------------------------------------------------
 echo -e "[\e[0;32m o.k. \x1B[0m] Building hostapd$TARGET"
 echo -e "[\e[0;32m o.k. \x1B[0m] Downloading dependencies."
-apt-get -qq -y install build-essential pkg-config libnl-3-dev libssl-dev libnl-genl-3-dev libnl*
+apt-get -qq -y install build-essential pkg-config libnl-3-dev libssl-dev libnl-genl-3-dev patchutils libnl*
 
 
 download ()
 {
 #--------------------------------------------------------------------------------------------------------------------------------
-# Download latest hostapd sources 
+# Download latest hostapd sources
 #--------------------------------------------------------------------------------------------------------------------------------
 if [ -d "$SRC/hostap" ]; then
 		cd $SRC/hostap
@@ -45,7 +44,7 @@ if [ -d "$SRC/hostap" ]; then
 		git pull -q
 		echo -e "[\e[0;32m o.k. \x1B[0m] Updating sources."
 	else
-		git clone -q http://w1.fi/hostap.git >> ../build.log 2>&1 
+		git clone -q http://w1.fi/hostap.git >> ../build.log 2>&1
 fi
 }
 
@@ -59,31 +58,6 @@ if [ "$1" == "stable" ]; then
 	git checkout -f -q "hostap_2_5" >> ../build.log 2>&1
 	else
 	git checkout -f -q >> ../build.log 2>&1
-fi
-}
-
-
-patching ()
-{
-#--------------------------------------------------------------------------------------------------------------------------------
-# Patching
-#
-# Other usefull patches:
-# https://dev.openwrt.org/browser/trunk/package/network/services/hostapd/patches?order=name
-#--------------------------------------------------------------------------------------------------------------------------------
-# brute force for 40Mhz
-if [ "$(patch --dry-run -t -p1 < $SRC/patch/300-noscan.patch | grep previ)" == "" ]; then
-	patch --batch -f -p1 < $SRC/patch/300-noscan.patch >> ../build.log 2>&1
-fi
-# patch for realtek
-if [ "$1" == "-realtek" ]; then
-		cp $SRC/config/config_realtek $SRC/hostap/hostapd/.config
-		patch --batch -f -p1 < $SRC/patch/realtek.patch >> ../build.log 2>&1
-	else
-		cp $SRC/config/config_default $SRC/hostap/hostapd/.config
-		if [ "$(cat $SRC/hostap/hostapd/main.c | grep rtl871)" != "" ]; then
-			patch --batch -t -p1 < $SRC/patch/realtek.patch >> ../build.log 2>&1
-		fi
 fi
 }
 
@@ -103,7 +77,6 @@ fi
 
 # download inside chroot fails
 if [ "$(stat -c %d:%i /)" == "$(stat -c %d:%i /proc/1/root/.)" ]; then
-echo "Outside chroot"
 download
 checkout "stable"
 fi
@@ -113,14 +86,14 @@ fi
 #--------------------------------------------------------------------------------------------------------------------------------
 # Read version
 VERSION=$(cat $SRC/hostap/src/common/version.h | grep "#define VERSION_STR " | awk '{ print $3 }' | sed 's/\"//g')
-cp $SRC/files/*.* $SRC/hostap/src/drivers/
 cd $SRC/hostap/
+for i in $SRC/patch/*.patch; do
+	lsdiff -s --strip=1 $i | grep '^+' | awk '{print $2}' | xargs -I % sh -c 'rm -f %'
+	patch -p1 -s --batch < $i
+	if [ $? -ne 0 ]; then echo -e "[\e[0;31m err. \x1B[0m] hostapd not built."; exit 1; fi
+done
+cp $SRC/config/config_realtek $SRC/hostap/hostapd/.config
 cd hostapd
-patching "-realtek"
-compiling " for Realteks"
-mv "$SRC/hostap/hostapd/hostapd" $SRC/hostap/hostapd/hostapd-rt
-mv "$SRC/hostap/hostapd/hostapd_cli" $SRC/hostap/hostapd/hostapd_cli-rt
-patching ""
 compiling
 
 #--------------------------------------------------------------------------------------------------------------------------------
@@ -128,27 +101,28 @@ compiling
 #--------------------------------------------------------------------------------------------------------------------------------
 cd $SRC
 apt-get -qq -d install hostapd
-dpkg-deb -R /var/cache/apt/archives/hostapd* hostapd-armbian${TARGET}"_"${REVISION}_${ARCHITECTURE}
+dpkg-deb -R /var/cache/apt/archives/hostapd* hostapd-custom${TARGET}"_"${REVISION}_${ARCHITECTURE}
 
 # set up control file
-cat <<END > hostapd-armbian${TARGET}_${REVISION}_${ARCHITECTURE}/DEBIAN/control
-Package: hostapd-armbian$TARGET
+cat <<END > hostapd-custom${TARGET}_${REVISION}_${ARCHITECTURE}/DEBIAN/control
+Package: hostapd-custom$TARGET
 Version: $REVISION
 Architecture: $ARCHITECTURE
 Maintainer: $MAINTAINER <$MAINTAINERMAIL>
 Installed-Size: 1
 Section: kernel
 Conflicts: hostapd
+Replaces: hostapd
 Priority: optional
 Description: Sources: https://github.com/igorpecovnik/hostapd
 END
 #
 
-cp "$SRC/hostap/hostapd/hostapd" "$SRC/hostap/hostapd/hostapd-rt" "$SRC/hostap/hostapd/hostapd_cli" "$SRC/hostap/hostapd/hostapd_cli-rt"  hostapd-armbian${TARGET}_${REVISION}_${ARCHITECTURE}/usr/sbin
-cp $SRC/hostapd.conf/*.conf hostapd-armbian${TARGET}_${REVISION}_${ARCHITECTURE}/etc
-cd hostapd-armbian${TARGET}_${REVISION}_${ARCHITECTURE}
+cp "$SRC/hostap/hostapd/hostapd" "$SRC/hostap/hostapd/hostapd_cli" hostapd-custom${TARGET}_${REVISION}_${ARCHITECTURE}/usr/sbin
+cp $SRC/hostapd.conf/*.conf hostapd-custom${TARGET}_${REVISION}_${ARCHITECTURE}/etc
+cd hostapd-custom${TARGET}_${REVISION}_${ARCHITECTURE}
 find . -type f ! -regex '.*.hg.*' ! -regex '.*?debian-binary.*' ! -regex '.*?DEBIAN.*' -printf '%P ' | xargs md5sum > DEBIAN/md5sums
 cd ..
-dpkg -b hostapd-armbian${TARGET}_${REVISION}_${ARCHITECTURE} >/dev/null 2>&1
-rm -rf hostapd-armbian${TARGET}_${REVISION}_${ARCHITECTURE}
-echo -e "[\e[0;32m o.k. \x1B[0m] All done. Hostapd is packed into: hostapd-armbian${TARGET}_${REVISION}_${ARCHITECTURE}.deb"
+dpkg -b hostapd-custom${TARGET}_${REVISION}_${ARCHITECTURE} >/dev/null 2>&1
+rm -rf hostapd-custom${TARGET}_${REVISION}_${ARCHITECTURE}
+echo -e "[\e[0;32m o.k. \x1B[0m] All done. Hostapd is packed into: hostapd-custom${TARGET}_${REVISION}_${ARCHITECTURE}.deb"
